e8c670f71e118185704d72ff1fd1608fe4ff6c06
Your Name <you@example.com>
2021-01-31 20:25:45 +0100

    first commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+checksum="da7766775e3f9c98d7a9145429f2be8297c2672fe5b118fd3dc2411fb48e0032"
+filename="bitcoin-0.21.0-x86_64-linux-gnu.tar.gz"
+wget "https://bitcoin.org/bin/bitcoin-core-0.21.0/bitcoin-0.21.0-x86_64-linux-gnu.tar.gz"
+test="$(echo "$checksum $filename" | sha256sum -c)"
+if [[ "$test" != "$filename: OK" ]]; then
+	exit 1
+fi
+tar xvf $filename
+install -m 0755 -o root -g root -t /usr/local/bin bitcoin-0.21.0/bin/*
